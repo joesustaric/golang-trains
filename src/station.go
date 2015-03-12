ec90399917a2e249cc82ae73bf9d6de32f848f0b
Joe Sustaric <jsustari@thoughtworks.com>
2015-03-12 21:18:23 +1100

    adding connectiing stations with distance

--- src/station.go
+++ src/station.go
@@ -1,5 +1,23 @@
 package trains
 
 type station struct {
-	name string
+	name        string
+	connections map[*station]int
+}
+
+func (s station) AddConnection(con *station, distance int) {
+	s.connections[con] = distance
+}
+
+func (s station) GetConnection(name string) station {
+	for k := range s.connections {
+		if k.name == name {
+			return *k
+		}
+	}
+	return station{"none", map[*station]int{}}
+}
+
+func (s station) GetDistanceTo(conn *station) int {
+	return s.connections[conn]
 }
--- src/station_test.go
+++ src/station_test.go
@@ -6,18 +6,35 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestPlay(t *testing.T) {
+func TestStation(t *testing.T) {
 
 	Convey("Given a station name 'F'", t, func() {
 		name := "F"
 
 		Convey("When we create a new station", func() {
-			station := station{name}
+			station := station{name, map[*station]int{}}
 
 			Convey("The station should be created correctly", func() {
 				So(station.name, ShouldEqual, "F")
 			})
 
+		})
+	})
+
+	Convey("Given a station object", t, func() {
+		name := "Foo"
+		foo := station{name, map[*station]int{}}
+
+		Convey("When we add a station it connects to", func() {
+			conectionName := "Bar"
+			bar := station{conectionName, map[*station]int{}}
+			distance := 5
+			foo.AddConnection(&bar, distance)
+
+			Convey("The station keeps a refrence to the connection with its distance", func() {
+				So(foo.GetConnection(conectionName), ShouldResemble, bar)
+				So(foo.GetDistanceTo(&bar), ShouldEqual, 5)
+			})
 		})
 	})
 }
