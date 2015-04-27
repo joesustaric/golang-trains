6732a582493f0db12818178bbc087c9ed0809a0d
Joe Sustaric <jsustari@thoughtworks.com>
2015-04-27 21:54:47 +1000

    using question test case for stops calculation and fixing a condition that slipped through the cracks

--- src/stops.go
+++ src/stops.go
@@ -15,7 +15,7 @@ const firstHop int = 1
 const singleHop int = 1
 
 // GetNumberOfRoutes balh
-func GetNumberOfRoutes(n Network, thq tripHopsQuery) int {
+func GetNumberOfRoutes(n *Network, thq tripHopsQuery) int {
 	origin, ok := n.GetNode(thq.origin)
 	if !ok {
 		return 0
@@ -24,14 +24,14 @@ func GetNumberOfRoutes(n Network, thq tripHopsQuery) int {
 	if !ok {
 		return 0
 	}
-	tripNodes := trip{from: origin, to: dest} //why dont I need to use &?
+	tripNodes := trip{from: origin, to: dest}
 	return numberOfRoutes(tripNodes, firstHop, thq.maxHops)
 }
 
 func numberOfRoutes(t trip, hopsTravelled, maxHops int) int {
 	result := 0
 	for conn := range t.from.connections {
-		if conn == t.to {
+		if conn == t.to || result < maxHops {
 			result++
 		} else if hopsTravelled < maxHops {
 			connTripNodes := trip{from: conn, to: t.to}
--- src/stops_test.go
+++ src/stops_test.go
@@ -9,29 +9,23 @@ import (
 func TestGetTripsBetween(t *testing.T) {
 
 	Convey("Given a network of stations", t, func() {
-		network := NewNetwork()
-		network.AddNode(makeStations()...)
-		Convey("When we ask for trips between with stop constraint of 3", func() {
+		network := getTestNetworkOfTrains()
+
+		Convey("When we ask for trips between C-C with stop constraint of 3", func() {
 			t := tripHopsQuery{origin: "C", destination: "C", maxHops: 3}
 			result := GetNumberOfRoutes(network, t)
 			Convey("It returns an array with all the correct routes", func() {
 				So(result, ShouldEqual, 2)
 			})
 		})
-	})
-}
 
-func makeStations() []*station {
-	b := station{"B", map[*station]int{}}
-	c := station{"C", map[*station]int{}}
-	d := station{"D", map[*station]int{}}
-	e := station{"E", map[*station]int{}}
-	result := []*station{&b, &c, &d, &e}
-	c.AddConnection(&d, 8)
-	c.AddConnection(&e, 2)
-	d.AddConnection(&e, 6)
-	d.AddConnection(&c, 8)
-	e.AddConnection(&b, 3)
-	b.AddConnection(&c, 4)
-	return result
+		Convey("When we ask for trips between A-C with stop constraint of 4", func() {
+			t := tripHopsQuery{origin: "A", destination: "C", maxHops: 4}
+			result := GetNumberOfRoutes(network, t)
+			Convey("It returns an array with all the correct routes", func() {
+				So(result, ShouldEqual, 3)
+			})
+		})
+	})
+
 }
